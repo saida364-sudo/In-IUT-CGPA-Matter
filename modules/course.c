5ebab9526cba57b196b6a0d6ed0de9d83342363a
Saida Nawal <saidangoungoure364@gmail.com>
2026-07-11 02:29:17 +0600

    Added dynamic allocation support for course lists

--- modules/course.c
+++ modules/course.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h> 
 #include "course.h"
 
-Course createCourse(char code[], char name[], double credit)
+Course createCourse(char code[], char name[], double credit, int semester)
 {
     Course course;
     strcpy(course.code, code);
     strcpy(course.name, name);
     course.credit = credit;
+    course.semester = semester;
     return course;
 }
 
@@ -16,4 +18,16 @@ void viewCourse(Course course)
     printf("Code: %s\n", course.code);
     printf("Name: %s\n", course.name);
     printf("Credit: %.1f\n", course.credit);
+    printf("Semester: %d\n", course.semester);
+}
+
+Course* createCourseList(int capacity)
+{
+    Course* list = (Course*)malloc(capacity * sizeof(Course));
+    return list;
+}
+
+void freeCourseList(Course* list)
+{
+    free(list);
 }
