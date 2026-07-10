dc721b1f4db46b94d07373b5bfe1f7915516192c
Saida Nawal <saidangoungoure364@gmail.com>
2026-07-11 01:17:25 +0600

    Added GPA module tests

--- test.ps1
+++ test.ps1
@@ -3,3 +3,6 @@ gcc -I modules modules\tests\test_course.c modules\course.c -o modules\tests\tes
 
 gcc -I modules modules\tests\test_course_result.c modules\course.c modules\courseResult.c -o modules\tests\test_course_result.exe
 .\modules\tests\test_course_result.exe
+
+gcc -I modules modules\tests\test_gpa.c modules\course.c modules\courseResult.c modules\gpa.c -o modules\tests\test_gpa.exe
+.\modules\tests\test_gpa.exe
