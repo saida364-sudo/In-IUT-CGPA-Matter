0f922f3fdebc1ee1888b94cc497d2486f480a3fb
Saida Nawal <saidangoungoure364@gmail.com>
2026-07-11 01:43:04 +0600

    Added tests for incomplete courses

--- modules/tests/test_gpa.c
+++ modules/tests/test_gpa.c
@@ -10,10 +10,26 @@ int testCGPA()
         createCourse("CSE 4108", "Structured Programming I Lab", 1.5)
     };
     CourseResult results[2] = {
-        createCourseResult(&courses[0], 240), // 80% -> 4.00
-        createCourseResult(&courses[1], 105)  // 70% -> 3.50
+        createCourseResult(&courses[0], 240, 1), // 80% -> 4.00
+        createCourseResult(&courses[1], 105, 1)  // 70% -> 3.50
     };
     double expected = ((4.00 * 3.0) + (3.50 * 1.5)) / 4.5;
+    double actual = calculateGPA(results, 2);
+    return (actual - expected < 0.001) && (expected - actual < 0.001);
+}
+
+int testIncompleteCourse()
+{
+    Course courses[2] = {
+        createCourse("CSE 4107", "Structured Programming I", 3.0),
+        createCourse("CSE 4108", "Structured Programming I Lab", 1.5)
+    };
+    
+    CourseResult results[2] = {
+        createCourseResult(&courses[0], 240, 1),
+        createCourseResult(&courses[1], 0, 0)
+    };
+    double expected = 4.00; 
     double actual = calculateGPA(results, 2);
     return (actual - expected < 0.001) && (expected - actual < 0.001);
 }
@@ -26,6 +42,9 @@ int main()
 
     total++;
     if (testCGPA()) passed++;
+
+    total++;
+    if (testIncompleteCourse()) passed++;
 
     printf("Passed %d/%d tests\n", passed, total);
     if (passed == total) return 0;
